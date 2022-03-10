924341fbc12155d27ed25f6842304af5ceb2ba9b
Jannes Frenker <frerib.j@gmail.com>
2022-03-10 12:45:47 +0100

    test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ all_led = [14, 15, 18, 23, 24, 25, 8, 7]
 controll_button = [16, 20]
 player_button = [26, 19, 13, 6, 5]
 
-for i in controll_led:
+for i in all_led:
     GPIO.setup(i, GPIO.OUT)
 
 try:
