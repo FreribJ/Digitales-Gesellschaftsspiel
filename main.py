619edcc0c7566f84fdc48c6880eeb15eb234fc83
Jannes Frenker <frerib.j@gmail.com>
2022-03-10 12:44:47 +0100

    test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 
 controll_led = [14, 15, 18]
 player_led = [23, 24, 25, 8, 7]
-all_led = player_led.append(controll_led)
+all_led = [14, 15, 18, 23, 24, 25, 8, 7]
 
 controll_button = [16, 20]
 player_button = [26, 19, 13, 6, 5]
