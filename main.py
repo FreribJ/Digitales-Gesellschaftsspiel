34d0d61b7c654adf9dba9aebe408e57f8be61927
Jannes Frenker <frerib.j@gmail.com>
2022-03-10 14:08:29 +0100

    test

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ GPIO.setmode(GPIO.BCM)
 
 controll_led = [14, 15, 18]
 player_led = [23, 24, 25, 8, 7]
-all_led = numpy.concatenate(controll_led, player_led)   #controll_led [14, 15, 18, 23, 24, 25, 8, 7]
+all_led = numpy.concatenate((controll_led, player_led))   #controll_led [14, 15, 18, 23, 24, 25, 8, 7]
 
 controll_button = [16, 20]
 player_button = [26, 19, 13, 6, 5]
