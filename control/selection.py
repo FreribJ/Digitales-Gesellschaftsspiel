f5a968f0fde229c07908ea54d2a2f8a021ff02ce
Jannes Frenker <frerib.j@gmail.com>
2022-03-11 21:54:53 +0100

    development

--- control/selection.py
+++ control/selection.py
@@ -18,8 +18,7 @@ def playerselection():
         GPIO.output(i, 1)
 
     #Event-Detect
-    for i in setup.all_button:
-        GPIO.add_event_detect(i, GPIO.RISING, bouncetime=1000)
+    GPIO.add_event_detect(setup.control_button[1], GPIO.RISING, bouncetime=1000)
 
     while not (GPIO.event_detected(setup.control_button[0]) or GPIO.event_detected(setup.control_button[1])):
         for i in setup.player_button:
