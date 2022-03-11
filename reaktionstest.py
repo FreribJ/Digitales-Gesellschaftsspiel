1d6e37d6494e9d9d5256cecb5c7799a2551da7a8
Jannes Frenker <frerib.j@gmail.com>
2022-03-11 11:39:49 +0100

    test

--- reaktionstest.py
+++ reaktionstest.py
@@ -1,33 +1,45 @@
-#Other Import
+# Other Import
 import time
+import random
 
-#GPIO Import
+import setup
+import animations
+
+# GPIO Import
 try:
     import RPi.GPIO as GPIO
 except ImportError:
     import FakeRPi.GPIO as GPIO
 
-#Variable Import
-import setup
+# Variablen
 
-#Variablen
-zeit = [0, 0, 0, 0, 0]
 
 def callback_zeitspeichern(switch):
-    print("knopf: ", switch)
-    print(time.time())
-    print()
+    player = setup.player_button.index(switch)
+    if not zeit[player] == 0:
+        zeit[player] = time.time()
+
 
 def initialize_callback():
     for switch in setup.player_button:
-        GPIO.add_event_detect(switch, GPIO.FALLING, callback_zeitspeichern, 200)
+        GPIO.add_event_detect(switch, GPIO.RISING, callback_zeitspeichern, 200)
+
 
 def remove_callback():
     for i in setup.player_button:
         GPIO.remove_event_detect(i)
 
+
 def start_reaktionstest():
+    global zeit
+    global count_pressed
     initialize_callback()
-    while True:
-        time.sleep(5)
-        print("Warte")
+    animations.blink(random.randint(2, 7))
+    count_pressed = 0
+    zeit = [0, 0, 0, 0, 0]
+    while count_pressed < len(setup.all_button):
+        time.sleep(1)
+    animations.rolls(setup.player_led, 1)
+    for t in zeit:
+        print(t)
+
