a07bbdab927459a84941b2e0eb71d69402174531
jan1310 <jan.schiffer@hsw-stud.de>
2022-03-11 16:42:25 +0100

    test

--- games/hotPotato.py
+++ games/hotPotato.py
@@ -12,21 +12,21 @@ except ImportError:
 from control import setup
 from helper import animations
 
-actualPlayer = random.randint(0, 4)
-GPIO.output(actualPlayer, 1)
+actualPlayer = random.randint(0, setup.active_player)
+GPIO.output(setup.active_led[actualPlayer], 1)
 timeLength = random.uniform(15, 30);
 startTime = time.time()
 
 def changePlayer():
     global actualPlayer
-    GPIO.output(actualPlayer, 0)
-    GPIO.remove_event_detect(setup.playerButton[actualPlayer])
+    GPIO.output(setup.active_led[actualPlayer], 0)
+    GPIO.remove_event_detect(setup.activeButton[actualPlayer])
     actualPlayer = random.randint(0, 4)
-    GPIO.output(actualPlayer, 1)
-    GPIO.add_event_detect(setup.playerButton[actualPlayer], GPIO.BOTH)
+    GPIO.output(setup.active_led[actualPlayer], 1)
+    GPIO.add_event_detect(setup.activeButton[actualPlayer], GPIO.BOTH)
 
 def startGame():
-    GPIO.add_event_detect(setup.playerButton[actualPlayer], GPIO.BOTH)
+    GPIO.add_event_detect(setup.activeButton[actualPlayer], GPIO.BOTH)
     while time.time()-startTime <= timeLength:
         if GPIO.event_detected(actualPlayer):
             changePlayer()
