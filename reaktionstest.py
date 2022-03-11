3a5ae18367ccfe80e6ec23b32fe7bd925d73c6ad
Jannes Frenker <frerib.j@gmail.com>
2022-03-11 11:13:13 +0100

    test

--- reaktionstest.py
+++ reaktionstest.py
@@ -20,7 +20,7 @@ def callback_zeitspeichern(switch):
 
 def initialize_callback():
     for switch in setup.player_button:
-        GPIO.add_event_detect(switch, GPIO.FALLING, callback=callback_function)
+        GPIO.add_event_detect(switch, GPIO.FALLING, callback=callback_zeitspeichern)
 
 def remove_callback():
     for i in setup.player_button:
