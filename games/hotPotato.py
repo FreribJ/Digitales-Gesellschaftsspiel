c5a418b2862037d5ef697b922d2f170340aec2f0
jan1310 <jan.schiffer@hsw-stud.de>
2022-03-12 12:56:03 +0100

    test

--- games/hotPotato.py
+++ games/hotPotato.py
@@ -27,19 +27,20 @@ def changePlayer():
     global actualPlayer
     GPIO.output(setup.active_led[actualPlayer], 0)
     GPIO.remove_event_detect(setup.active_button[actualPlayer])
-    actualPlayer = random.randint(0, setup.active_player-1) #-1 da randint sonst einen zu viel macht
+    actualPlayer = random.randint(0, setup.active_player-1)
     GPIO.output(setup.active_led[actualPlayer], 1)
     GPIO.add_event_detect(setup.active_button[actualPlayer], GPIO.BOTH)
 
 def startGame():
-    initializeGame() #Damit die werte erst beim Start gesetzt werden
+    initializeGame()
 
     GPIO.add_event_detect(setup.active_button[actualPlayer], GPIO.BOTH)
     while time.time()-startTime <= timeLength:
-        if GPIO.event_detected(setup.active_button[actualPlayer]): #Es fehlt das: setup.active_button...
+        if GPIO.event_detected(setup.active_button[actualPlayer]):
             changePlayer()
-    animations.all_blink(5, 0.3)
-    animations.one_blink(setup.active_led[actualPlayer], 5, 0.3)
+    if setup.areAllPlayerAlive():
+        setup.subtractLifeFromPlayer(actualPlayer)
+        startGame()
 
     for i in setup.active_button:
         GPIO.remove_event_detect(i)
