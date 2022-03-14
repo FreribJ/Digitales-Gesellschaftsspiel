22d0e3f59cd7b8ab5eb180f150e7abf35b175fc9
Jannes Frenker <frerib.j@gmail.com>
2022-03-14 11:02:51 +0100

    beauty

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ except ImportError:
     import FakeRPi.GPIO as GPIO
 
 def callbackEndGame(switch):
-    for i in len(setup.player_life):
+    for i in range(len(setup.player_life)):
         setup.player_life[i] = 1
 
 #Programm-Initialize:
