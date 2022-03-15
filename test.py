53cf0ac4144e1e35f87a17d28378f388f1e72542
Jannes Frenker <frerib.j@gmail.com>
2022-03-15 18:26:58 +0100

    continue

---
+++ test.py
@@ -0,0 +1,13 @@
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    import FakeRPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.input())
+
+GPIO.add_event_detect(20, GPIO.RISING, 200)
+
+while True:
+    print(GPIO.event_detected(20))
