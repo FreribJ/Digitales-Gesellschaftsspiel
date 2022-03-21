8fd3fb6078096eaec85e430e269c01b2b1981adb
finn <finn-matthis.veerkamp@hsw-stud.de>
2022-03-21 20:29:01 +0100

    finn

--- games/zaehlen.py
+++ games/zaehlen.py
@@ -12,16 +12,49 @@ except ImportError:
 
 global ran_num
 
-#Zeiten Speichern
+nummer = []
+
+#Nummern Speichern
 def callback_zeitspeichern(switch):
-    zeit = time.time()
-    global zeiten
+    global nummer
 
     player = setup.active_button.index(switch)
-    if zeiten[player] == 0:
-        zeiten[player] = zeit
+    if nummer[player] == i:
+        nummer[player] = i+1
+    nummer [player] = max(i, ran_num) - min(i, ran_num)
 
 #Initialzes Callback
 def initialize_callback():
     for switch in setup.active_button:
         GPIO.add_event_detect(switch, GPIO.RISING, callback_zeitspeichern, 200)
+
+def startGame():
+    #Vorbereiten
+    ran_num =  random.randint(9, 21)
+    global nummer
+    initialize_callback()
+
+    while setup.areAllPlayerAlive():
+        nummer = []
+        #Warten
+        animations.all_blink(1, random.randint(2, 5))
+        # Blinken
+        while t < ran_num:
+            animations.player_led(random.randint(0,4),1,random.randint(150, 450))
+            t=t+1
+        #Start
+        for i in range(setup.active_player):
+            zeiten.append(0)
+        #Auf Ende Warten
+        time.sleep(8)
+        while nummer.count(0) > 0:
+            time.sleep(1)
+
+        #Gewinner/Verlierer berechnen
+        winner = zeiten.index(min(nummer))
+        loser = zeiten.index(max(nummer))
+
+        setup.subtractLifeFromPlayerWithWinner(loser, winner)
+
+    #Ende
+    setup.remove_callback()
