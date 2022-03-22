192d75178afe061e5d2c7843a19d13c85a73da73
Jannes Frenker <frerib.j@gmail.com>
2022-03-22 16:20:34 +0100

    new setup

--- control/setup.py
+++ control/setup.py
@@ -16,14 +16,14 @@ player_life = []
 
 game_selected = 0
 
-control_led = [14, 15, 18]
-control_button = [16, 20]
-player_led = [23, 24, 25, 8, 7]
-player_button = [26, 19, 13, 6, 5]
+control_led = [22, 27, 17]
+control_button = [4, 3]
+player_led = [14, 18, 24, 8, 12, 20, 26, 13, 5, 9]
+player_button = [15, 23, 25, 7, 16, 21, 19, 6, 11, 10]
 max_player = len(player_button)
 
-all_led = [14, 15, 18, 23, 24, 25, 8, 7]
-all_button = [16, 20, 26, 19, 13, 6, 5]
+all_led = [22, 27, 17, 14, 18, 24, 8, 12, 20, 26, 13, 5, 9]
+all_button = [4, 3, 15, 23, 25, 7, 16, 21, 19, 6, 11, 10]
 
 #16 -> Back; 20 -> Next
 active_player = 0
