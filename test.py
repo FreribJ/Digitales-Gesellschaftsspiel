e16898fba70bfac72a212ef1f26823ae53f7d705
Jannes Frenker <frerib.j@gmail.com>
2022-03-22 23:03:22 +0100

    testing

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-import time
-for i in range(10):
-    print(time.time())
-    time.sleep(1)
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    import FakeRPi.GPIO as GPIO
+
+all_button = [3, 4, 15, 23, 25, 7, 16, 21, 19, 6, 11, 10]
+
+for i in all_button:
+    print(i, " -> ", all_button.index(i))
+    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
