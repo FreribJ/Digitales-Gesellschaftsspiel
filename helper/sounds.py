76d3404df4ddd21d5a34d5423e28bcaabd80bed9
Jannes Frenker <frerib.j@gmail.com>
2022-03-30 00:32:29 +0200

    fixed dir

--- helper/sounds.py
+++ helper/sounds.py
@@ -8,27 +8,27 @@ def initialize():
     mixer.init()
 
 def playLoseSound():
-    music.load("Sounds/lose.wav")
+    music.load("helper/Sounds/lose.wav")
     music.set_volume(1)
     music.play()
 
 def playMenuSound():
-    music.load("Sounds/menuSelection.mp3")
+    music.load("helper/Sounds/menuSelection.mp3")
     music.set_volume(1)
     music.play()
 
 def playButtonPush():
-    music.load("Sounds/buttonpush.mp3")
+    music.load("helper/Sounds/buttonpush.mp3")
     music.set_volume(0.5)
     music.play()
 
 def playSound(Soundfile):
-    music.load("Sounds/" + Soundfile)
+    music.load("helper/Sounds/" + Soundfile)
     music.set_volume(1)
     music.play()
 
 def playSoundAtPosition(Soundfile, Startposition):
-    music.load("Sounds/" + Soundfile)
+    music.load("helper/Sounds/" + Soundfile)
     music.set_volume(1)
     music.play(0, Startposition)
 
@@ -37,7 +37,8 @@ def stopSound():
 
 
 #test:
-
+initialize()
+playButtonPush()
 
 #https://soundbible.com/419-Tiny-Button-Push.html
 #https://www.youtube.com/watch?v=T9N0pmLI7Jw
