1f03bf67b1a5f72387da285980256b92069c21a0
Jannes Frenker <frerib.j@gmail.com>
2022-03-30 21:09:54 +0200

    changed order

--- control/setup.py
+++ control/setup.py
@@ -11,12 +11,12 @@ except ImportError:
 #3 -> Back; 4 -> Next
 control_button = [3, 4]
 control_led = [22, 27, 17]
-player_led = [14, 18, 24, 8, 12, 20, 26, 13, 5, 9]
-player_button = [15, 23, 25, 7, 16, 21, 19, 6, 11, 10]
+player_led = [5, 9, 14, 18, 24, 8, 12, 20, 26, 13]
+player_button = [11, 10, 15, 23, 25, 7, 16, 21, 19, 6]
 max_player = len(player_button)
 
-all_led = [22, 27, 17, 14, 18, 24, 8, 12, 20, 26, 13, 5, 9]
-all_button = [3, 4, 15, 23, 25, 7, 16, 21, 19, 6, 11, 10]
+all_led = [22, 27, 17, 5, 9, 14, 18, 24, 8, 12, 20, 26, 13]
+all_button = [3, 4, 11, 10, 15, 23, 25, 7, 16, 21, 19, 6]
 
 active_player = 0
 active_button = []
