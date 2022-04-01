64f1782e1aa72537ac680515b4c9f50555e01335
Jannes Frenker <frerib.j@gmail.com>
2022-04-01 17:42:59 +0200

    testing bug

--- test.py
+++ test.py
@@ -7,9 +7,11 @@ except ImportError:
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(11, GPIO.IN)
+GPIO.setup(5, GPIO.OUT)
 
 GPIO.add_event_detect(11, GPIO.RISING, bouncetime=200)
 
 while True:
+    GPIO.output(5, 1)
     if GPIO.event_detected(11):
         print("event detected: ")
