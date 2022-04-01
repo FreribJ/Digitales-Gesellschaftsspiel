650de02d90305430c666892dba36dd706c8ac34b
Jannes Frenker <frerib.j@gmail.com>
2022-04-01 16:05:25 +0200

    testing bug

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
-import time
-
-from pygame import mixer
-from pygame.mixer import music
-from control import setup
-# GPIO Import
 try:
     import RPi.GPIO as GPIO
 except ImportError:
     import FakeRPi.GPIO as GPIO
 
-mixer.init()
-music.load("helper/Sounds/buttonpush.mp3")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(11, GPIO.IN)
 
+GPIO.add_event_detect(11, GPIO.RISING)
 
-def callback(pin):
-    music.play()
-
-GPIO.add_event_detect(setup.player_button[0], GPIO.RISING, callback, 200)
-
-time.sleep(1000)
+while True:
+    print("status: " + GPIO.input(11))
+    print("event detected: " + GPIO.event_detected())
