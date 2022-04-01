81c5f9adf3a6965d32cf31a13bbdb0e164607925
Jannes Frenker <frerib.j@gmail.com>
2022-04-01 17:04:26 +0200

    testing bug

--- control/setup.py
+++ control/setup.py
@@ -34,7 +34,7 @@ def initialize():
     for i in all_led:
         GPIO.setup(i, GPIO.OUT)
     for i in all_button:
-        GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(i, GPIO.IN)
 
 #Removes Callback
 def remove_callback():
