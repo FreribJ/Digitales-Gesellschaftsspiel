fa49756d970a84b4337cf228ad8b6e73dee67ab0
Jannes Frenker <frerib.j@gmail.com>
2022-04-01 17:54:26 +0200

    testing bug

--- test.py
+++ test.py
@@ -5,16 +5,18 @@ try:
 except ImportError:
     import FakeRPi.GPIO as GPIO
 
+led = 9
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(11, GPIO.IN)
-GPIO.setup(5, GPIO.OUT)
+GPIO.setup(led, GPIO.OUT)
 
 GPIO.add_event_detect(11, GPIO.BOTH, bouncetime=200)
 
 while True:
-    GPIO.output(5, 1)
+    GPIO.output(led, 1)
     if GPIO.event_detected(11):
         print("event detected: ")
-    GPIO.output(5, 0)
+    GPIO.output(led, 0)
     if GPIO.event_detected(11):
         print("event detected: ")
