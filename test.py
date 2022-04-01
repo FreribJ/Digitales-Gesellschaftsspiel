3e3ac3485aeb30de9bbc528f064c6d5b6649ca27
Jannes Frenker <frerib.j@gmail.com>
2022-04-01 17:49:03 +0200

    testing bug

--- test.py
+++ test.py
@@ -13,8 +13,10 @@ GPIO.add_event_detect(11, GPIO.BOTH, bouncetime=200)
 
 while True:
     GPIO.output(5, 1)
+    print(GPIO.input(11))
     if GPIO.event_detected(11):
         print("event detected: ")
     GPIO.output(5, 0)
+    print(GPIO.input(11))
     if GPIO.event_detected(11):
         print("event detected: ")
