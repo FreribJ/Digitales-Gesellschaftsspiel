179e7a35383cd0f4673cea9af241f9ffca17ffe9
RealMaggi <buxmag@hotmail.com>
2022-04-04 10:59:53 +0200

    Magnus

---
+++ games/zeitSchätzen.py
@@ -0,0 +1,18 @@
+import time
+import random
+
+from control import setup
+from helper import animations, sounds
+
+# GPIO Import
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    import FakeRPi.GPIO as GPIO
+
+# Variablen
+
+#Initialzes Callback
+def initializeGame():
+    for switch in setup.active_button:
+        GPIO.add_event_detect(switch, GPIO.RISING, bouncetime=800)
