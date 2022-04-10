e2eddd48557cc5701ba9e8d325055dd1f95304fa
RealMaggi <buxmag@hotmail.com>
2022-04-10 13:29:04 +0200

    Magnus

--- games/zeitSchätzen.py
+++ games/zeitSchätzen.py
@@ -18,3 +18,7 @@ def initialize_callback():
     for switch in setup.active_button:
         GPIO.add_event_detect(switch, GPIO.RISING, callback_zeitspeichern, 200)
 
+# Pseudocode:
+# Kurze Erklärung wie das Spiel funktionieren soll: Nach der Spieler- und Lebensauswahl beginnen die Spielrunden. Die LEDs leuchten eine zufällige Anzahl an Sekunden (Wahrscheinlich am besten zwischen 5 und 30 Sekunden oder sowas)
+# Nachdem die LEDs wieder aus sind, können die Spieler schätzen wie lange die LEDs wohl geleuchtet haben und ihren Guess in Sekunden durch Knopfdruck hochzählen. 1 Kmopfdruck = 1 Sekunde. Am Ende verlieren Die die am weitesten
+# von der Tatsächlichen Zahl entfernt waren ein Leben.
