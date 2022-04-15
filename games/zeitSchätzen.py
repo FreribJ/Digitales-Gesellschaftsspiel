1153f527ef7723b8c8aad50eb43e1d720bd4e784
Jannes <frerib.j@gmail.com>
2022-04-15 19:43:53 +0200

    fixing bugs

--- games/zeitSchätzen.py
+++ games/zeitSchätzen.py
@@ -34,7 +34,7 @@ def startGame():
     while setup.areAllPlayerAlive():
         #Setup
         zeitZuSchaetzen = random.randint(2, min(10, setup.max_player))
-        animations.one_blink(setup.all_led[zeitZuSchaetzen-1], 1, 2)
+        animations.one_blink(setup.player_led[zeitZuSchaetzen-1], 1, 2)
         time.sleep(1)
 
         # Start
