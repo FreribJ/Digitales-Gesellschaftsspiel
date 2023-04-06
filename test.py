9e255d180369a079bef9f04eebeb6db7b309f990
Jannes Frenker <frerib.j@gmail.com>
2023-04-06 18:49:12 +0200

    testing

--- test.py
+++ test.py
@@ -10,8 +10,14 @@ GPIO.setmode(GPIO.BOARD)
 for i in range(40):
     try:
         print("Trying Pin ", i)
-        GPIO.setup(i, GPIO.OUT)
-        GPIO.output(i, GPIO.LOW)
+        GPIO.setup(i, GPIO.IN)
+        GPIO.add_event_detect(i, GPIO.RISING, bouncetime=300)
         time.sleep(4)
     except:
         print(i, " ist kein Output")
+
+while True:
+    for i in range(40):
+        if GPIO.event_detected(i):
+            print("Es ist: ", i)
+    time.sleep(1)
