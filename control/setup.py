5b21f039106f6736bae1c56f0cb19b6aa55fa775
Jannes Frenker <frerib.j@gmail.com>
2023-04-06 18:06:39 +0200

    testing

--- control/setup.py
+++ control/setup.py
@@ -15,10 +15,10 @@ WAIT_FOR_CONTINUE = True
 #all_button = [3, 4, 11, 10, 15, 23, 25, 7, 16, 21, 19, 6]
 
 ##For Banana Pi M2 Zero:
-all_button = [5, 7, 23, 19, 10, 16, 22, 26, 36, 40, 35, 31]
-all_led = [15, 13, 11, 29, 21, 8, 12, 18, 24, 32, 38, 37, 33]
+all_button = [23, 31, 0, 0, 0, 10, 0, 0, 0, 0, 19, 0]
+all_led = [23, 31, 11, 29, 21, 8, 12, 18, 24, 32, 38, 37, 33]
 
-#3 -> Back; 4 -> Next
+#First -> Back; Second -> Next
 control_button = [all_button[0], all_button[1]]
 control_led = [all_led[0], all_led[1], all_led[2]]
 player_led = [all_led[3], all_led[4], all_led[5], all_led[6], all_led[7], all_led[8], all_led[9], all_led[10], all_led[11], all_led[12]]
