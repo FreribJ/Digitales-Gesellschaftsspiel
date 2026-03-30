017b5756ba92ed5e1530609e39986cd99a6d2c35
Frerib_J <frerib.j@gmail.com>
2026-03-30 23:12:02 +0200

    further testing pigpio

--- main.py
+++ main.py
@@ -10,31 +10,30 @@ from control.setup import pi
 setup.initialize()
 
 #Programm-Start:
-try:
-    #Startanimation:
-    print("Started")
-    animations.rolls(setup.all_led, 1)
 
-    menu_level = 0
+#Startanimation:
+print("Started")
+animations.rolls(setup.all_led, 1)
+
+menu_level = 0
+exit_check = 0
+
+print("Spielerauswahl")
+pi.write(setup.control_led[0], 1)
+next_menu = selection.player_selection()
+sounds.playMenuSound()
+animations.rolls(setup.player_led, 1)
+pi.write(setup.control_led[0], 0)
+if next_menu:
+    menu_level = 1
     exit_check = 0
+else:
+    #Auf Standard Resetten:
+    setup.active_led = []
+    setup.active_button = []
+    setup.game_selected = 0
+    setup.max_life = 1
+    exit_check += 1
+    if exit_check >= 3:
+        print("exit")
 
-    print("Spielerauswahl")
-    pi.write(setup.control_led[0], 1)
-    next_menu = selection.player_selection()
-    sounds.playMenuSound()
-    animations.rolls(setup.player_led, 1)
-    pi.write(setup.control_led[0], 0)
-    if next_menu:
-        menu_level = 1
-        exit_check = 0
-    else:
-        #Auf Standard Resetten:
-        setup.active_led = []
-        setup.active_button = []
-        setup.game_selected = 0
-        setup.max_life = 1
-        exit_check += 1
-        if exit_check >= 3:
-            print("exit")
-
-pi.stop()
