2f7561c724139eb5fbf5f2dcf1d62ba78638cdfc
Frerib_J <frerib.j@gmail.com>
2026-03-30 23:30:46 +0200

    further testing pigpio

--- control/selection.py
+++ control/selection.py
@@ -25,7 +25,7 @@ def player_selection():
 
     # Event-Detect
     for i in setup.all_button:
-        pi.allback(i, pigpio.EITHER_EDGE, player_selected_callback)
+        pi.callback(i, pigpio.EITHER_EDGE, player_selected_callback)
 
     abbruch = False
     event_detector.add_event_detect(setup.control_button[0], 32, bouncetime=300)
